dc17248f9ec2d15a9adcf5c5d15dde523e90adb3
Tommy <tommy.requillard@laposte.net>
2023-02-06 14:36:04 +0100

    list render wip

--- js/utils/listRender.js
+++ js/utils/listRender.js
@@ -13,10 +13,12 @@ function createIngredients(recipes) {
     recipes.forEach(recipe => recipe.ingredients.forEach(i => map.set(i.ingredient, '')))
 
     let html = ''
-    map.forEach((value, ingredient) => html += `<li class="col-4"><button class="border-0 bg-primary text-white m-2 p-1 text-start w-100">${ingredient}</button></li>`)
+    map.forEach((value, ingredient) => html += `<li class="col-4">
+                                                <button class="border-0 bg-primary text-white m-2 p-1 text-start w-100">${ingredient}
+                                                </button>
+                                                </li>`)
 
     wrapper.innerHTML = html
-
 
     const buttonToDisplayList = document.getElementById('ingredientsButton')
     buttonToDisplayList.addEventListener('click', (event) => {
@@ -33,7 +35,7 @@ function createIngredients(recipes) {
         event.target.parentNode.getElementsByTagName('ul')[0].classList.remove('d-none')
 
         onClickOut(
-            document.getElementById("ingredientsButton").parentNode,
+            document.getElementById('ingredientsButton').parentNode,
             () => {
                 event.target.classList.remove('d-none')
                 event.target.parentNode.getElementsByTagName('input')[0].classList.add('d-none')
@@ -41,7 +43,6 @@ function createIngredients(recipes) {
             }
         )
     })
-
 }
 
 function createAppliances(recipes) {
@@ -51,8 +52,35 @@ function createAppliances(recipes) {
     recipes.forEach(recipe => map.set(recipe.appliance, ''))
 
     let html = ''
-    map.forEach((value, appliance) => html += `<li><a href="#">${appliance}</li>`)
+    map.forEach((value, appliance) => html += `<li class="col-4">
+                                                <button class="border-0 bg-success text-white m-2 p-1 text-start w-100">${appliance}
+                                                </button>
+                                                </li>`)
     wrapper.innerHTML = html
+
+    const buttonToDisplayList = document.getElementById('appliancesButton')
+    buttonToDisplayList.addEventListener('click', (event) => {
+        // cache le bouton
+        event.target.classList.add('d-none')
+
+        const input = event.target.parentNode.getElementsByTagName('input')[0]
+        // affiche le input
+        input.classList.remove('d-none')
+        // done le focus
+        input.focus()
+
+        // affiche la liste
+        event.target.parentNode.getElementsByTagName('ul')[0].classList.remove('d-none')
+
+        onClickOut(
+            document.getElementById('appliancesButton').parentNode,
+            () => {
+                event.target.classList.remove('d-none')
+                event.target.parentNode.getElementsByTagName('input')[0].classList.add('d-none')
+                event.target.parentNode.getElementsByTagName('ul')[0].classList.add('d-none')
+            }
+        )
+    })
 }
 
 function createUstensils(recipes) {
@@ -62,6 +90,33 @@ function createUstensils(recipes) {
     recipes.forEach(recipe => recipe.ustensils.forEach(ustensil => map.set(ustensil, '')))
 
     let html = ''
-    map.forEach((value, ustensil) => html += `<li><a href="#">${ustensil}</li>`)
+    map.forEach((value, ustensil) => html += `<li class="col-4">
+                                                <button class="border-0 bg-danger text-white m-2 p-1 text-start w-100">${ustensil}
+                                                </button>
+                                                </li>`)
     wrapper.innerHTML = html
+
+    const buttonToDisplayList = document.getElementById('ustensilsButton')
+    buttonToDisplayList.addEventListener('click', (event) => {
+        // cache le bouton
+        event.target.classList.add('d-none')
+
+        const input = event.target.parentNode.getElementsByTagName('input')[0]
+        // affiche le input
+        input.classList.remove('d-none')
+        // done le focus
+        input.focus()
+
+        // affiche la liste
+        event.target.parentNode.getElementsByTagName('ul')[0].classList.remove('d-none')
+
+        onClickOut(
+            document.getElementById('ustensilsButton').parentNode,
+            () => {
+                event.target.classList.remove('d-none')
+                event.target.parentNode.getElementsByTagName('input')[0].classList.add('d-none')
+                event.target.parentNode.getElementsByTagName('ul')[0].classList.add('d-none')
+            }
+        )
+    })
 }
